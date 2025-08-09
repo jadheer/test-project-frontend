e808dec39919e0211704758605cc71a791c7c58d
Jadheer Ahammed <ajadheer@gmail.com>
2025-08-09 08:51:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch('https://www.ineedtotest.work.gd/api/data')  // 👈 Replace with your EC2 or backend URL
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+      .catch((err) => console.error(err))
+  }, [])
+
+  return (
+    <div>
+      <h1>React App</h1>
+      <p>{data}</p>
+    </div>
+  )
+}
+
+export default App
